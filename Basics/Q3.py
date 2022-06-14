3638c044c9c51d3ac6c8ebdf69080544be016a44
SYED JAVEED MATEEN <104886848+syedjaveed18@users.noreply.github.com>
2022-06-14 11:52:09 +0530

    Perfect Square
    Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
    Sample Testcase :
    INPUT
    5 5
    OUTPUT
    yes

---
+++ Basics/Q3.py
@@ -0,0 +1,11 @@
+import math
+n,m = input().split()
+n = int(n)
+m = int(m)
+p = n*m
+#if int(math.sqrt(p)) == math.sqrt(p) :
+z = math.sqrt(p)
+if int(z)-z == 0 :
+    print("yes")
+else:
+    print("no")
