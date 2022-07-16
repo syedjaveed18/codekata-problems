d843ce4bfac3f9e83ac5cc00306c677b6a53bfeb
SYED JAVEED MATEEN <104886848+syedjaveed18@users.noreply.github.com>
2022-07-16 22:52:17 +0530

    number of elements in the array such that A[i]+A[j]=A[k] and i<j<k
    Given an array of N elements, find the elements A[i], A[j] and A[k] in the array such that A[i]+A[j]=A[k] and i < j < k . Print number of possibilities.
    Input Size : 1 <= N <= 1000
    Sample Testcases :
    INPUT
    5
    1 2 3 4 5
    OUTPUT
    4

---
+++ Arrays/Q209.py
@@ -0,0 +1,11 @@
+n = int(input())
+array = list(map(int,input().split()))
+
+count = 0
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if array[i]+array[j] == array[k]:
+                count += 1
+
+print(count)
